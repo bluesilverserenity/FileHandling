a059b45af3f3056aeb1e2abe2ecf4c25b0f406e1
bluesilverserenity <idamunyad@gmail.com>
2026-05-04 14:58:00 +0100

    delaying output

--- delay.py
+++ delay.py
@@ -5,5 +5,8 @@ mydates = []
 for i in range(5):
     mydates.append(datetime.datetime.now())
     time.sleep(2)  # Sleep for 2 seconds to create a delay between each datetime entry
-    print(mydates[i])  # Print the current datetime entry after the delay
-    
+    print(mydates[i]) # Print the current datetime entry after the delay
+print("\n")
+
+for i in mydates:  # Print the current datetime entries stored after the delay
+    print(i)
